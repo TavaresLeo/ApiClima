56b151f56166809d0452b2b72b2cd7da63eab714
Leonardo Tavaress <106924333+TavaresLeo@users.noreply.github.com>
2022-12-30 19:43:47 -0300

    primeiro API

---
+++ js/scripts.js
@@ -0,0 +1,74 @@
+//Variáveis e seleção de elementos
+const apiKey = "544174bd2bfe8116425360c0dbda71cb";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+const loader = document.querySelector("#loader");
+
+const weatherContainer = document.querySelector("#weather-data")
+
+const toggleLoader = () => {
+    loader.classList.toggle("hide");
+};
+
+const getWeatherData = async(city) => {
+    toggleLoader();
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json()
+
+    toggleLoader();
+
+    return(data);
+};
+
+searchBtn.addEventListener("click", async (e) => {
+    console.log();
+})
+
+const showWeatherData = async (city) => {
+
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove("hide");
+}
+
+// Eventos
+searchBtn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    
+    const city = cityInput.value;
+
+    showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+    if (e.code === "Enter") {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
+});
